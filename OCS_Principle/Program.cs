67f4318ade18a5f1da50d784462393ccc1d239f1
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2024-03-09 14:15:41 +0200

    Add project files.

---
+++ OCS_Principle/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace OCS_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            CustomerSilver customerSilver = new CustomerSilver();
+            Console.WriteLine(customerSilver.GetDiscount(300)); ;
+
+            GoldCustomer goldCustomer = new GoldCustomer();
+            Console.WriteLine(goldCustomer.GetDiscount(300));
+        }
+    }
+    /// <summary>
+    /// the “Customer” class is now closed 
+    /// for any new modification 
+    /// but it’s open for extensions when
+    /// new customer types are added to the project.
+    /// </summary>
+    public class Customer
+    {
+        public virtual double GetDiscount(double TotalSales)
+        {
+            return TotalSales;
+        }
+    }
+    public class CustomerSilver : Customer
+    {
+        public override double GetDiscount(double TotalSales)
+        {
+            return base.GetDiscount(TotalSales) - 100;
+        }
+    }
+    public class GoldCustomer : Customer
+    {
+        public override double GetDiscount(double TotalSales)
+        {
+            return base.GetDiscount(TotalSales) - 50;
+        }
+    }
+}
